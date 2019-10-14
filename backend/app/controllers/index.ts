20581772e55b6ea68e52af17422f99e3966ebba2
JANISKUNZ <janis.kunz@students.unibe.ch>
2019-10-14 13:21:30 +0200

    Backend Start
    add db3 sqlite file with the user Database
    login.html and register2.html small html file for testing
    user.model sequlize code that represents user table
    server2.ts
    login controller not fully functional
    register controller needs some work

--- backend/app/controllers/index.ts
+++ backend/app/controllers/index.ts
@@ -1,3 +1,5 @@
 export * from './todolist.controller';
 export * from './todoitem.controller';
 export * from './welcome.controller';
+export * from './login.controller';
+export * from './register.controller';
---
+++ backend/app/server2.ts
@@ -0,0 +1,48 @@
+import {Sequelize} from 'sequelize-typescript';
+import {User} from './models/user.model';
+import {LoginController} from './controllers/login.controller';
+import {RegisterController} from './controllers/Register.controller';
+import express from 'express';
+
+const bodyParser = require('body-parser');
+const sequelize =  new Sequelize({
+    database: 'development',
+    dialect: 'sqlite',
+    username: 'root',
+    password: '',
+    storage: 'db3.sqlite'
+});
+sequelize.addModels([User]);
+
+
+// create a new express application instance
+const app: express.Application = express();
+app.use(express.json());
+app.use(express.static('app'))
+app.use(bodyParser.urlencoded());
+// define the port the express app will listen on
+var port: number = 3000;
+if (process.env.PORT !== undefined) {
+    port = parseInt(process.env.PORT);
+}
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.use('/login.html', LoginController );
+app.use('/register2.html', RegisterController );
+
+
+
+
+sequelize.sync().then(() => {
+// start serving the application on the given port
+    app.listen(port, () => {
+        // success callback, log something to console as soon as the application has started
+        console.log(`Listening at http://localhost:${port}/`);
+    });
+});
