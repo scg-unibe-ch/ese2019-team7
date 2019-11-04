35278cda8550a5c353fabac4ebe1b20b84fbc11e
Michael Dooley <mdooley-ch@live.com>
2019-11-04 14:07:19 +0100

    Fixed display, added search function in frontend

--- frontend/src/app/offer-list/offer-list.component.ts
+++ frontend/src/app/offer-list/offer-list.component.ts
@@ -10,24 +10,53 @@ import {OfferItem} from '../offer-item';
 export class OfferListComponent implements OnInit {
 
   @Input()
-  offerItem: OfferItem = new OfferItem('', '', 0, '', 0, 0);
+  offerItem: OfferItem = new OfferItem('', '', '', '', '', '');
   offerItems: OfferItem[] = [];
+
+  categories = ['food & drink', 'entertainment', 'location', null];
+
+  searchKey = '';
+  category = '';
 
   constructor(
     private httpClient: HttpClient
   ) { }
 
   ngOnInit() {
-    this.httpClient.get('http://localhost:3000/offers', {
-    }).subscribe((instances: any) => {
-      this.offerItems = instances.map((instance) => new OfferItem(
-        instance.title,
-        instance.description,
-        instance.price,
-        instance.category,
-        instance.dateFrom,
-        instance.dateTo));
+    this.httpClient.get('http://localhost:3000/offers').subscribe((instances: any) => {
+      this.offerItems = this.generateOfferItems(instances);
     }, (object: any) => {  alert('HTTP Error ' + object.status + ': ' + object.error.message); });
   }
 
+  generateOfferItems(instances: any) {
+    return instances.offers.map((instance) => new OfferItem(
+      instance.title,
+      instance.description,
+      this.generatePriceDisplay(instance.price),
+      instance.category,
+      instance.dateFrom,
+      instance.dateTo));
+  }
+
+  generatePriceDisplay(price: number) {
+    if (price == null) {
+      return 'N/A';
+    } else {
+      return String(price);
+    }
+  }
+  generateDateDisplay(date: number) {
+    if (date == null) {
+      return 'N/A';
+    } else {return String(date); }
+  }
+
+  onSearch() {
+    this.httpClient.put('http://localhost:3000/search', {
+      searchKey: this.searchKey,
+      category: this.category
+    }).subscribe((instances: any) => {
+      this.offerItems = this.generateOfferItems(instances);
+    }, (object: any) => {  alert('HTTP Error ' + object.status + ': ' + object.error.message); });
+  }
 }
