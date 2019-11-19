b1a54cbfc3f049c699465432704b6ea5fc96b1e2
Maksim Fomin <maksim.fomin@students.unibe.ch>
2019-11-19 15:30:15 +0100

    Integrated active element to the navbar

--- frontend/src/app/navigation-bar/navigation-bar.component.ts
+++ frontend/src/app/navigation-bar/navigation-bar.component.ts
@@ -10,6 +10,28 @@ export class NavigationBarComponent implements OnInit {
 
   constructor(private variables: VariablesService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    // Get the container element
+    let listContainer = document.getElementById('ul');
+
+// Get all buttons with class="li" inside the container
+    let entries = listContainer.getElementsByClassName('li');
+
+// Loop through the list entries and add the active class to the current/clicked entry
+    for (let i = 0; i < entries.length; i++) {
+      entries[i].addEventListener('click', function() {
+        let current = document.getElementsByClassName('active');
+
+        // If there's no active class
+        if (current.length > 0) {
+          current[0].className = current[0].className.replace(' active', '');
+        }
+
+        // Add the active class to the current/clicked list entry
+        this.className += ' active';
+      });
+    }}
+
+
 
 }
