1c007a0013a0d769214cdf2709e8136a442e48a9
Michael Dooley <mdooley-ch@live.com>
2019-11-27 14:55:45 +0100

    Registering now outputs a string rather than undefined; navigates to offers if not logged in previously

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -3,6 +3,7 @@ import {RegistrationUser} from '../registration-user';
 import {HttpClient} from '@angular/common/http';
 import {VariablesService} from '../variables.service';
 import {FunctionsService} from '../functions.service';
+import {Router} from '@angular/router';
 
 
 @Component({
@@ -15,16 +16,20 @@ export class RegisterComponent implements OnInit {
   constructor(
     private httpClient: HttpClient,
     private variables: VariablesService,
-    private functions: FunctionsService
+    private functions: FunctionsService,
+    private router: Router
   ) { }
 
   model = new RegistrationUser('', '', '', '');
 
   submitted = false;
+  isLoggedIn = false;
 
   onSubmit() {this.submitted = true; }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.variables.getLogin().subscribe(login => this.isLoggedIn = login);
+  }
 
   /**
    * Sends a registration request to backend. Checks if user is logged in upon success.
@@ -36,8 +41,11 @@ export class RegisterComponent implements OnInit {
       email: this.model.email,
       tel: this.model.tel,
       address: this.model.address
-    }, {withCredentials: true}).subscribe( (object: any) => {
-      this.variables.checkLoginStatus();
-      this.functions.answer(object); }, this.functions.onSave_error);
+    }, {withCredentials: true}).subscribe( (object: any) => { if (!this.isLoggedIn) {
+      this.router.navigate(['offers']);
+    }
+                                                              this.variables.checkLoginStatus();
+                                                              alert('registration complete'); },
+      this.functions.onSave_error);
   }
 }
